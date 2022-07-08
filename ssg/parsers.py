e48b672b6d5083d3ea01d020329e2953d2890bb2
pyramidlip <108816216+pyramidlip@users.noreply.github.com>
2022-07-08 14:51:43 +0200

    typos

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -42,7 +42,7 @@ class MarkdownParser(Parser):
     def parse(self, path: Path, source: Path, dest: Path):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        self.write(path, dest, content)
+        self.write(path, dest, html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
 class ReStructuredTextParser(Parser):
@@ -50,5 +50,6 @@ class ReStructuredTextParser(Parser):
 
     def parse(self, path: Path, source: Path, dest: Path):
         content = Content.load(self.read(path))
-        publish_parts(content.body, writer_name='html5')
+        html = publish_parts(content.body, writer_name='html5')
+        self.write(path, dest, html["html_body"])
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
