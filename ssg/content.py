c74e76311c6f6025a32f5bc024ae7f1d0a4310b5
pyramidlip <108816216+pyramidlip@users.noreply.github.com>
2022-07-08 14:05:23 +0200

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -4,13 +4,13 @@ from collections.abc import Mapping
 
 
 class Content(Mapping):
-    __delimeter = "^(?:-|\+){3}\s*$"
-    __regex = re.compile(__delimeter, re.MULTILINE)
+    __delimiter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
 
     @classmethod
     def load(cls, string):
         _, fm, content = cls.__regex.split(string, 2)
-        metadata = load(fm ,Loader=FullLoader)
+        metadata = load(fm, Loader=FullLoader)
         return cls(metadata, content)
 
     def __init__(self, metadata, content):
@@ -19,21 +19,21 @@ class Content(Mapping):
 
     @property
     def body(self):
-        self.data["content"]
+        return self.data["content"]
 
     @property
     def type(self):
         return self.data["type"] if "type" in self.data else None
 
     @type.setter
-    def type(self, new_type):
-        self.data["type"] = new_type
+    def type(self, type):
+        self.data["type"] = type
 
-    def __getitem__(self, item):
-        return self.data[item]
+    def __getitem__(self, key):
+        return self.data[key]
 
     def __iter__(self):
-        return self.data
+        return self.data.__iter__()
 
     def __len__(self):
         return len(self.data)
@@ -41,7 +41,6 @@ class Content(Mapping):
     def __repr__(self):
         data = {}
         for key, value in self.data.items():
-            if key is not "content":
+            if key != "content":
                 data[key]: value
         return str(data)
-    
