f92cdebc59c2234d581562d6fd186134df302cff
pyramidlip <108816216+pyramidlip@users.noreply.github.com>
2022-07-08 14:02:06 +0200

    Update content.py

---
+++ ssg/content.py
@@ -0,0 +1,47 @@
+import re
+from yaml import FullLoader, load
+from collections.abc import Mapping
+
+
+class Content(Mapping):
+    __delimeter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter, re.MULTILINE)
+
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+        metadata = load(fm ,Loader=FullLoader)
+        return cls(metadata, content)
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data["content"] = content
+
+    @property
+    def body(self):
+        self.data["content"]
+
+    @property
+    def type(self):
+        return self.data["type"] if "type" in self.data else None
+
+    @type.setter
+    def type(self, new_type):
+        self.data["type"] = new_type
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+    def __iter__(self):
+        return self.data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        data = {}
+        for key, value in self.data.items():
+            if key is not "content":
+                data[key]: value
+        return str(data)
+    
